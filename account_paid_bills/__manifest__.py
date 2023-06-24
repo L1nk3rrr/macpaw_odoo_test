7ec3d4cf09bce350f46d34afec0ee921d577d4aa
ValentynStarushok <valentyn@enapps.co.uk>
2023-06-24 18:49:28 +0300

    [MPO-#1] [DEV] macpaw_odoo_test: initial commit for account_paid_bills

---
+++ account_paid_bills/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Account Paid Bills',
+    'version' : '1.0',
+    # 'category': '',
+    'depends' : ['account'],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
